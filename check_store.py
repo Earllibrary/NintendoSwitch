1cb5f71259b4bb7d049fc4c58582e63da90fd1a2
kenmikanmi <ken-h5526@live.jp>
2018-03-19 19:34:26 +0900

    Update and rename SOLDOUT.py to check_store.py

--- check_store.py
+++ check_store.py
@@ -12,7 +12,7 @@ ninURL = "https://store.nintendo.co.jp/category/NINTENDOSWITCH/HAC_S_KAYAA.html"
 #notify via LINE if SWITCH is available
 def message_LINE(message):
 	
-	yourToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	yourToken = "get_your_own_token_and_paste_here"
 	
 	line_notify_api = 'https://notify-api.line.me/api/notify'
 
@@ -20,24 +20,22 @@ def message_LINE(message):
 	headers = {'Authorization': 'Bearer ' + line_notify_token}  
 	line_notify = requests.post(line_notify_api, data=payload, headers=headers)
                            
-#check stock of nintendo store
+#check nintendo store
 def stock_NtdStr():
 	s = subprocess.getoutput("curl "+ninURL)
 	if("SOLD OUT" in s):
-		print("sold out "+datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+		# print("sold out "+datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 		return False
 	else:
 		print("now on sale  "+datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+                return True
 
 if __name__ == "__main__":
 
 	N = 30
-	ninFlag = -1	#if flag=1, switch is on sale
-	yodFlag = -1
 
-	b = 1
 	try:
-		while b==1:
+		while True:
 			if(stock_NtdStr()):
 				message_LINE("SWITCH now on sale!\nat\n"+ninURL)
 			time.sleep(N)
